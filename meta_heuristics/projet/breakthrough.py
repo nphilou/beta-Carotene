3e3afd978a518ce459cb515b01edeeda1eeb5bc9
philippe <nphilou@gmail.com>
2019-02-19 00:25:21 +0100

    tomatrix

--- meta_heuristics/projet/breakthrough.py
+++ meta_heuristics/projet/breakthrough.py
@@ -6,6 +6,7 @@ from sys import stdin, stdout, stderr
 
 Dx = 5
 Dy = 5
+SizePolicy = 3 * Dx * Dy
 
 Empty = 0
 White = 1
@@ -29,7 +30,7 @@ class Board(object):
     def __init__(self):
         self.turn = White
         self.h = 0
-        self.board = np.zeros((Dx, Dy))
+        self.board = np.zeros((Dx, Dy), dtype=int)
         for i in range(0, 2):
             for j in range(0, Dy):
                 self.board[i][j] = White
@@ -44,14 +45,14 @@ class Board(object):
             for j in range(0, Dy):
                 self.board[i][j] = b.board[i][j]
 
-    def legalMoves(self, color):
+    def legalMoves(self):
         moves = []
         for i in range(0, Dx):
             for j in range(0, Dy):
-                if self.board[i][j] == color:
+                if self.board[i][j] == self.turn:
                     for k in [-1, 0, 1]:
                         for l in [-1, 0, 1]:
-                            m = Move(color, i, j, i + k, j + l)
+                            m = Move(self.turn, i, j, i + k, j + l)
                             if m.valid(self):
                                 moves.append(m)
         return moves
@@ -99,12 +100,12 @@ class Board(object):
         return False
 
     def playout(self):
-        l = self.legalMoves(self.turn)
+        l = self.legalMoves()
         while len(l) > 0 and not self.won(White) and not self.won(Black):
             indice = int(random.random() * len(l))
             m = l[indice]
             self.play(m)
-            l = self.legalMoves(self.turn)
+            l = self.legalMoves()
         if len(l) == 0:
             if self.turn == Black:
                 return 1.0
@@ -116,11 +117,11 @@ class Board(object):
             return 0.0
 
     def flat(self, n):
-        l = self.legalMoves(self.turn)
+        l = self.legalMoves()
         best = l[0]
-        bestScore = n + 1
+        best_score = n + 1
         if self.turn == White:
-            bestScore = -1.0
+            best_score = -1.0
         b = Board()
         for m in l:
             sum = 0
@@ -129,27 +130,36 @@ class Board(object):
                 b.play(m)
                 r = b.playout()
                 sum = sum + r
-            if (self.turn == White and sum > bestScore) or (self.turn == Black and sum < bestScore):
-                bestScore = sum
+            if (self.turn == White and sum > best_score) or (self.turn == Black and sum < best_score):
+                best_score = sum
                 best = m
         return best
 
-    def getOutputNetwork(self, turn):
+    def to_matrix(self):
+        bitmap = np.zeros((3, Dx, Dy), dtype=int)
 
-        return 0, 75 * [0.0]
+        for i in range(Dx):
+            for j in range(Dy):
+                bitmap[self.board[i][j]][i][j] = 1
 
+        return bitmap
+
+
+    def getOutputNetwork(self):
+        if self.turn == White:
+            return (SizePolicy + 1) * [1.0]
+        else:
+            return (SizePolicy + 1) * [1.0]
 
     def descent(self, TT):
         if self.h not in TT:
-            v, prior = self.getOutputNetwork(self.turn)
-            # v, prior = 0, 75 * [0.0]
+            output = self.getOutputNetwork()
             l = []
-            l.append(75 * [0])
-            l.append(75 * [0.0])
-            l.append(v)
-            l.append(prior)
+            l.append(SizePolicy * [0])
+            l.append(SizePolicy * [0.0])
+            l.append(output)
             TT[self.h] = l
-            return v
+            return output[Dx * Dy]
         elif self.won(White):
             return 1.0
         elif self.won(Black):
@@ -157,25 +167,25 @@ class Board(object):
         else:
             s = TT[self.h]
             turn = self.turn
-            l = self.listeCoupsPossibles()
+            l = self.legalMoves()
             if len(l) == 0:
                 if turn == White:
                     return 0.0
                 return 1.0
 
             best = l[0]
-            bestScore = -1000000000.0
-            sumPlayouts = 0
-            sumScores = 0.0
+            best_score = -1000000000.0
+            sum_playouts = 0
+            sum_scores = 0.0
 
             for m in l:
                 i = m.code()
-                sumPlayouts = sumPlayouts + s[0][i]
-                sumScores = sumScores + s[1][i]
+                sum_playouts = sum_playouts + s[0][i]
+                sum_scores = sum_scores + s[1][i]
 
             mean = 0.0
-            if sumPlayouts > 0:
-                mean = sumScores / sumPlayouts
+            if sum_playouts > 0:
+                mean = sum_scores / sum_playouts
 
             for m in l:
                 i = m.code()
@@ -187,9 +197,10 @@ class Board(object):
                 if turn == Black:
                     mu = 1.0 - mu
 
-                puct = mu + 0.3 * s[3][i] * math.sqrt(sumPlayouts) / (1 + s[0][i])
-                if puct > bestScore:
-                    bestScore = puct
+                puct = mu + 0.3 * s[2][i] * math.sqrt(sum_playouts) / (1.0 + s[0][i])
+
+                if puct > best_score:
+                    best_score = puct
                     best = m
 
             self.play(best)
@@ -200,24 +211,21 @@ class Board(object):
             s[1][i] = s[1][i] + v
             return v
 
-    def PUCT(self, TT, nbPlayouts=800):
+    def PUCT(self, TT, nb_playouts=800):
         b = Board()
-        for i in range(nbPlayouts):
+        for i in range(nb_playouts):
             b.copy(self)
             b.descent(TT)
         s = TT[self.h]
-        l = self.listeCoupsPossibles()
+        l = self.legalMoves()
         best = l[0]
-        bestScore = -1000000000.0
+        best_score = -1000000000.0
         for m in l:
             i = m.code()
-            if s[0][i] > bestScore:
-                bestScore = s[0][i]
+            if s[0][i] > best_score:
+                best_score = s[0][i]
                 best = m
         return best
-
-    def listeCoupsPossibles(self):
-        return self.legalMoves(self.turn)
 
 
 class Move(object):
@@ -239,7 +247,7 @@ class Move(object):
                     return True
                 return False
             elif board.board[self.x2][self.y2] == Empty:
-                if (self.y2 == self.y1 + 1 or self.y2 == self.y1 - 1 or self.y2 == self.y1):
+                if self.y2 == self.y1 + 1 or self.y2 == self.y1 - 1 or self.y2 == self.y1:
                     return True
                 return False
         elif self.color == Black:
@@ -265,20 +273,22 @@ class Move(object):
             direction = 0
         if self.y2 < self.y1:
             direction = 2
-        return 3 * (5 * self.x1 + self.y1) + direction
+        return 3 * (Dx * self.x1 + self.y1) + direction
 
 
 if __name__ == '__main__':
-    b = Board()
-    b.print()
-    l = b.legalMoves(White)
-    b.printMoves(l)
-    r = b.playout()
-    print(r)
-
-    b = Board()
-    tt1 = {}
-    b.PUCT(tt1)
 
     # m = b.flat(100)  # < MCTS
     # print(m)
+    b = Board()
+    print(b.to_matrix())
+
+    #l = b.legalMoves()
+    #b.printMoves(l)
+    #r = b.playout()
+    #print(r)
+    #b = Board()
+    #m = b.flat(100)
+    #print(m)
+    #m = b.PUCT({})
+    #print(m)
