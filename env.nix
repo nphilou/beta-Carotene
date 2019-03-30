e25dcaa9009cbacd6f0b3319369f669c88e9018d
Philippe <nphilou@gmail.com>
2019-03-30 22:37:13 +0100

    update pyzmq to 18.0.1

--- env.nix
+++ env.nix
@@ -1,6 +1,22 @@
-with import <unstable> {};
+with import <unstable> {
+  config.allowUnfree = true;
+};
 
-python36.withPackages (
+
+let python = pkgs.python36.override {
+  packageOverrides = self: super: {
+    pyzmq = super.pyzmq.overridePythonAttrs(old: rec {
+      pname = "pyzmq";
+      version = "18.0.1";
+      src = super.fetchPypi {
+        inherit pname version;
+        sha256 = "8b319805f6f7c907b101c864c3ca6cefc9db8ce0791356f180b1b644c7347e4c";
+      };
+    });
+  };
+};
+
+in python.withPackages (
   ps: with ps; [
     numpy
     toolz
